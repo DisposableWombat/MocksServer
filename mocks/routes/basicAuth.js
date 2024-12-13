41a2ea243f73d9ce5b7359438f3a07578e2f1be5
Andrew McGuire <disposablewombat@gmail.com>
2024-12-13 11:49:53 -0700

    get the auth tests working

--- mocks/routes/basicAuth.js
+++ mocks/routes/basicAuth.js
@@ -2,20 +2,19 @@
 
 module.exports = [
 	{
-		id: "auth-test",
-		url: "/authtest",
+		id: "log-something",
+		url: "/authtest01",
 		method: "GET",
 		variants: [
-
 			{
-				id: "log-something",
+				id: "log",
 				type: "middleware",
 				options: {
 					middleware: (req, res) => {
 						// console.log("request: ", req);
 						// console.log("headers: ", req.rawHeaders);
 
-						console.log("Authorization: ", req.headers('Authorization'));
+						console.log("Authorization: ", req.header('Authorization'));
 
 						let strArray = req.header('Authorization').split(' ');
 						console.log("strArray: ", strArray);
@@ -23,12 +22,40 @@ module.exports = [
 						let decodedAuthString = atob(strArray[1]);
 						console.log("decodedAuthString: ", decodedAuthString);
 
-						let authArray = decodedAuthString.spit(':');
+						let authArray = decodedAuthString.split(':');
 						console.log("User: ", authArray[0]);
 						console.log("Password: ", authArray[1]);
 
 						res.status(200);
 						res.send("fnord");
+					}
+				}
+			}
+		]
+	},
+	{
+		id: "verify-login",
+		url: "/authtest02",
+		method: "GET",
+		variants: [
+			{
+				id: "verify",
+				type: "middleware",
+				options: {
+					middleware: (req, res) => {
+						let strArray = req.header('Authorization').split(' ');
+						let decodedAuthString = atob(strArray[1]);
+						let authArray = decodedAuthString.split(':');
+						// console.log("User: ", authArray[0]);
+						// console.log("Password: ", authArray[1]);
+
+						if (authArray[0] == "andyroo" && authArray[1] == "gooberpucky") {
+							res.status(200);
+							res.send("happy, happy");
+						} else {
+							res.status(401);
+							res.send("no soup for you");
+						}
 					}
 				}
 			}
